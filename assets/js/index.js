71ac7782b89d5f9df30f88a735108d22e463ef08
Nick Jabs <yabsikus@gmail.com>
2023-08-08 16:03:58 +0100

    Version 01, education purpose only

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+  let response = await fetch(
+    "https://hhmm5u7563ftso4tgzopolbrvu0kbwdj.lambda-url.eu-west-3.on.aws/"
+  );
+  let data = await response.json();
+  counter.innerHTML = ` This page has ${data} Views!`;
+}
+
+updateCounter();
