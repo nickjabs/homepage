efbe7c120fbe005da060c84110ec6d3cf97ccf9a
Nick Jabs <yabsikus@gmail.com>
2023-11-22 17:39:08 +0100

    updating skills, added devops-path, blog for my homepage

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,40 +1,95 @@
-// ... Your existing code ...
-
+// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-  const showPopupLinks = document.querySelectorAll(
-    ".content a[id^='showPopup']"
-  );
-  const closePopupButtons = document.querySelectorAll("[id^='closePopup']");
-  const projectPopups = document.querySelectorAll(".popup");
+  // Get references to the popup and close links
+  var showPopup1Link = document.getElementById("showPopup1");
+  var closePopup1Link = document.getElementById("closePopup1");
+  var projectPopup1 = document.getElementById("projectPopup1");
 
-  console.log("Script loaded");
+  // Function to show the popup
+  function showPopup1() {
+    projectPopup1.style.display = "block";
+  }
 
-  showPopupLinks.forEach((link, index) => {
-    link.addEventListener("click", (event) => {
+  // Function to close the popup
+  function closePopup1() {
+    projectPopup1.style.display = "none";
+  }
+
+  // Add a click event listener to the "show" link
+  if (showPopup1Link) {
+    showPopup1Link.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the link from jumping to the top
+      showPopup1();
+    });
+  }
+
+  // Add a click event listener to the "close" link
+  if (closePopup1Link) {
+    closePopup1Link.addEventListener("click", function (event) {
       event.preventDefault();
-      console.log(`Show popup ${index + 1} clicked`);
-      projectPopups[index].style.display = "block";
+      closePopup1();
     });
+  }
+
+  // Close the popup if the user clicks anywhere outside of it
+  window.addEventListener("click", function (event) {
+    if (event.target === projectPopup1) {
+      closePopup1();
+    }
   });
 
-  closePopupButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      console.log(`Close popup ${index + 1} clicked`);
-      projectPopups[index].style.display = "none";
+  // Close the popup if the user presses the Escape key
+  window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closePopup1();
+    }
+  });
+});
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the popup and close links for the second popup
+  var showPopup2Link = document.getElementById("showPopup2");
+  var closePopup2Link = document.getElementById("closePopup2");
+  var projectPopup2 = document.getElementById("projectPopup2");
+
+  // Function to show the second popup
+  function showPopup2() {
+    projectPopup2.style.display = "block";
+  }
+
+  // Function to close the second popup
+  function closePopup2() {
+    projectPopup2.style.display = "none";
+  }
+
+  // Add a click event listener to the "show" link for the second popup
+  if (showPopup2Link) {
+    showPopup2Link.addEventListener("click", function (event) {
+      event.preventDefault();
+      showPopup2();
     });
+  }
+
+  // Add a click event listener to the "close" link for the second popup
+  if (closePopup2Link) {
+    closePopup2Link.addEventListener("click", function (event) {
+      event.preventDefault();
+      closePopup2();
+    });
+  }
+
+  // Close the second popup if the user clicks anywhere outside of it
+  window.addEventListener("click", function (event) {
+    if (event.target === projectPopup2) {
+      closePopup2();
+    }
   });
 
-  // Additional event listener for the new popup (projectPopup3)
-  const newPopup = document.querySelector("#projectPopup3");
-  const newPopupShowLink = document.querySelector("#showPopup3");
-  const newPopupCloseButton = document.querySelector("#closePopup3");
-
-  newPopupShowLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    newPopup.style.display = "block";
-  });
-
-  newPopupCloseButton.addEventListener("click", () => {
-    newPopup.style.display = "none";
+  // Close the second popup if the user presses the Escape key
+  window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closePopup2();
+    }
   });
 });
